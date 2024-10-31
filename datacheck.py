9fd7f94a5257b483c22f2a2eca1fe7400c9ea836
josbeporcayo <josbeporcayo@gmail.com>
2024-10-30 20:16:40 -0600

    :sparkles: Se agrego la revision de enfermedades

--- datacheck.py
+++ datacheck.py
@@ -1,5 +1,5 @@
 #%%
-from sqlalchemy import create_engine,MetaData,Table,Column,Integer,String,DateTime,JSON
+from sqlalchemy import create_engine,MetaData,Table,Column,Integer,String,DateTime,JSON,ForeignKey
 import pandas as pd
 engine=create_engine('sqlite:///data.db')
 
@@ -12,7 +12,17 @@ paciente=Table('paciente',metadata,
     Column('birthdate',DateTime),
     Column('comorbilidades',JSON))
 
+enfermedades=Table('enfermedades',metadata,
+    Column('id',Integer,primary_key=True,autoincrement=True),
+    Column('enfermedad',String),
+    Column('startDate',String),
+    Column('endDate',String),
+    Column('complications',String),
+    Column('paciente_id',Integer,ForeignKey('paciente.id')))
+
 #%%
 df=pd.read_sql_table('paciente',con=engine)
 df
 # %%
+dfenfe=pd.read_sql_table('enfermedades',con=engine)
+dfenfe
